7ae6439f0a90b2092851011aa16fbc77d99c4627
aanzieu <anzieu.antoine@gmail.com>
2016-05-06 09:13:28 +0200

    newstrjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: aanzieu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/10 20:06:03 by aanzieu           #+#    #+#             */
-/*   Updated: 2016/03/15 23:11:32 by aanzieu          ###   ########.fr       */
+/*   Updated: 2016/05/06 09:12:26 by aanzieu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*new;
 
-	if (!(new = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1)))
+	if (!(new = (char*)malloc(sizeof(char)
+	* (ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (NULL);
-	if (new)
-	{
-		new = ft_strdup(s1);
-		new = ft_strcat(new, s2);
-		new[ft_strlen(new)] = '\0';
-	}
+	new = ft_strcpy(new, s1);
+	new = ft_strcat(new, s2);
 	return (new);
 }
